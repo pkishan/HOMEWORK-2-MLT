9d64594c1fdcd14c3d14fac3a734c8f61e915f52
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-09-29 14:02:37 +0530

    Multiple perceptron method for MNIST data set is done

--- PP1/perceptron_multiclass.m
+++ PP1/perceptron_multiclass.m
@@ -13,26 +13,44 @@ k = 0; % number of mistakes (i.e., number of updates)
 
 % Just do a single pass over the training data (our stopping condition will
 % simply be this)
+C= X_train';
+D = X_test';
 for n=1:N
     % TODO: predict the label for the n-th training example
-    % as y_pred = argmax W'x_n + b
+    x_n = C(:, n);
+    A = W'*x_n + b';
+    [maxL, ind] = max(A(:));
+    y_pred = ind;
     if y_pred ~= Y_train(n)
         k = k + 1;
         % TODO: Update W, b
-        
+        W(:,Y_train(n)) = W(:, Y_train(n)) + x_n;
+        b(Y_train(n)) = b(Y_train(n)) + 1;
+        W(:,y_pred) = W(:, y_pred) - x_n;
+        b(y_pred) = b(y_pred) - 1;
         % TODO: Update W_avg, b_avg using Ruppert Polyak Averaging
         % Important: You don't need to store all the previous W's and b's
         % to compute W_avg, b_avg
+        W_avg = W_avg - (W_avg - W)/k;
+        b_avg = b_avg - (b_avg - b)/k;
         
         % TODO: Predict test labels using W, b
-        y_test_pred = % ?? TODO
+        for i = 1:size(Y_test, 1)
+            x_i =  D(:,i); 
+            B = W'*x_i + b';
+            [maxA ,y_test_pred(i,1)] = max(B(:));   % ?? TODO
+        end
         acc(k) = mean(Y_test==y_test_pred);   % test accuracy     
         
         % TODO: Now predict test labels using W_avg, b_avg
-        y_test_pred = % ?? TODO
+        for i = 1:size(Y_test, 1)
+            x_i = D(:,i);
+            B = W_avg'*x_i + b_avg';
+            [maxA ,y_test_pred(i,1)] = max(B(:)) ;   % ?? TODO
+        end
         acc_avg(k) = mean(Y_test==y_test_pred); % test accuracy with R-P averaging
-        
-        fprintf('Update number %d, accuracy = %f, accuracy (with R-P averaging) = %f\n',k,acc(k),acc_avg(k));
+       
+       fprintf('Update number %d, accuracy = %f, accuracy (with R-P averaging) = %f\n',k,acc(k),acc_avg(k));
         plot(1:k,acc(1:k),'r');
         hold on;
         plot(1:k,acc_avg(1:k),'g');
